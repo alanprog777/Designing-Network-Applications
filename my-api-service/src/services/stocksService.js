494a90dc978cd77f1e8e52ca27c826af1caaae59
Алан <alan-ali.ali2017@yandex.ru>
2026-05-14 17:20:33 +0300

    done

--- my-api-service/src/services/stocksService.js
+++ my-api-service/src/services/stocksService.js
@@ -1,25 +1,25 @@
 const fileService = require('./fileService');
 
 class StocksService {
-    constructor() {
-        this.dataPath = '';
-    }
-
-    init(path) {
-        this.dataPath = path;
-    }
+    constructor() { this.dataPath = ''; }
+    init(path) { this.dataPath = path; }
 
     findAll(title) {
         const stocks = fileService.readData(this.dataPath);
-        if (title) {
-            return stocks.filter(s => s.title.toLowerCase().includes(title.toLowerCase()));
-        }
-        return stocks;
+        return title ? stocks.filter(s => s.title.toLowerCase().includes(title.toLowerCase())) : stocks;
     }
 
     findById(id) {
         const stocks = fileService.readData(this.dataPath);
         return stocks.find(s => s.id === parseInt(id));
+    }
+
+    delete(id) {
+        let stocks = fileService.readData(this.dataPath);
+        const initialLength = stocks.length;
+        stocks = stocks.filter(s => s.id !== parseInt(id));
+        fileService.writeData(this.dataPath, stocks);
+        return stocks.length !== initialLength;
     }
 
     create(data) {
@@ -32,60 +32,6 @@ class StocksService {
         fileService.writeData(this.dataPath, stocks);
         return newStock;
     }
-
-    delete(id) {
-        let stocks = fileService.readData(this.dataPath);
-        const initialLength = stocks.length;
-        stocks = stocks.filter(s => s.id !== parseInt(id));
-        fileService.writeData(this.dataPath, stocks);
-        return stocks.length !== initialLength;
-    }
-
-    update(id, data) {
-        const stocks = fileService.readData(this.dataPath);
-        const index = stocks.findIndex(s => s.id === parseInt(id));
-
-        if (index !== -1) {
-            stocks[index] = { ...stocks[index], ...data };
-
-            fileService.writeData(this.dataPath, stocks);
-
-            return stocks[index];
-        }
-        return null;
-    }
-
-    deleteUnpopular() {
-        const stocks = fileService.readData(this.dataPath);
-        const initialCount = stocks.length;
-        const filteredStocks = stocks.filter(s => s.members >= 10);
-
-        if (filteredStocks.length !== initialCount) {
-            fileService.writeData(this.dataPath, filteredStocks);
-        }
-
-        return {
-            deleted: initialCount - filteredStocks.length,
-            remaining: filteredStocks.length
-        };
-}
-
-    clone(id) {
-        const stocks = fileService.readData(this.dataPath);
-        const original = stocks.find(s => s.id == id);
-
-        if (!original) return null;
-
-        const newStock = {
-            ...original,
-            id: Date.now(),
-            title: `${original.title} (Копия)`
-        };
-
-        stocks.push(newStock);
-        fileService.writeData(this.dataPath, stocks);
-        return newStock;
-}
 }
 
 module.exports = new StocksService();
